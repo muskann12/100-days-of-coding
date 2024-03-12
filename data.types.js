a0a106b7e20d46b46473946562c97326bc6058e6
Muskan Nisar <159582953+muskann12@users.noreply.github.com>
2024-03-12 13:58:01 -0700

    Add files via upload

---
+++ data.types.js
@@ -0,0 +1,18 @@
+//string
+var message = "hello its me muskan here";
+console.log(message);
+//number
+var numA = 20;
+var numB = 50;
+console.log(numA + numB);
+//boolean
+var isPaid = false;
+console.log(isPaid);
+//Bigint
+var x = 92345897065740n;
+var y = 901235547088640n;
+var sum = x + y;
+console.log("sum of x and y", sum);
+//null
+var or = null;
+console.log(or);
